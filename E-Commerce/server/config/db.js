fe31dbbbf5d105f5cf38dd60acf21f4419b3c910
Gaawali150 <163647512+Gaawali150@users.noreply.github.com>
2025-05-08 22:52:03 +0530

    Update db.js

--- E-Commerce/server/config/db.js
+++ E-Commerce/server/config/db.js
@@ -5,15 +5,12 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI; // Ensure this is defined
+    const uri = process.env.MONGO_URI; 
     if (!uri) {
       console.error('❌ MONGO_URI is not defined!');
       process.exit(1);
     }
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(uri); 
     console.log('✅ MongoDB connected');
   } catch (error) {
     console.error('❌ MongoDB connection failed:', error.message);
@@ -22,4 +19,3 @@ const connectDB = async () => {
 };
 
 export default connectDB;
-
